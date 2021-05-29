06631fc6498fba6ef4c8c35b881bd6a8fa1fe5cd
nareshtarani <nareshtarani@gmail.com>
2021-05-29 07:01:58 +0000

    Added vpc module

---
+++ vpc.tf
@@ -0,0 +1,11 @@
+//--------------------------------------------------------------------
+// Modules
+module "network" {
+  source  = "app.terraform.io/nttarani-training/network/google"
+  version = "3.2.2"
+
+  network_name = "gaurav-network"
+  project_id = "var.project"
+  subnets = "subnets = [  
+       {     subnet_name   = "gaurav-subnet"     subnet_ip     = "10.100.10.0/24"     subnet_region = var.region   } ]"
+}
